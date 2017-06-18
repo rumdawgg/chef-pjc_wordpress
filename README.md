5f4e070bc958f06ce192874bde44819a709123b2
Paul Chicarello <paul@chicarello.com>
2017-06-18 18:51:00 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# pjc_wordpress
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: pjc_wordpress
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
